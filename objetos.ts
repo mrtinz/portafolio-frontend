4773c821aae062361548dfbfb32f681fc9c8b0f0
mrtinz <melmrtinz@gmail.com>
2022-07-16 16:32:00 -0300

    last update

--- objetos.ts
+++ objetos.ts
@@ -13,6 +13,17 @@ let portafolio:Persona={
     Titulos:["",""],
 }
 function GetNombre(nombre:string) {
-    
+    const nombre1 = document.getElementById("Nombre");
+    nombre1.textContent=nombre;
 }
+
+function GetApellido(apellido:string) {
+    const a = document.getElementById("Apellido");
+    a.textContent=apellido;
+    return portafolio.Apellido=apellido;
+}
+function GetAcercade(acercade:string) {
+    const a = document.getElementById("text-acercade");
+    a.textContent=acercade;
+    return portafolio.Acercade=acercade;
 }
