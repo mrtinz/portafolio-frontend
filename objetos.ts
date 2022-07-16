1e57ce0f7d73d30782e0c43fd633febb8b89b753
mrtinz <melmrtinz@gmail.com>
2022-07-16 15:11:28 -0300

    new changes

---
+++ objetos.ts
@@ -0,0 +1,18 @@
+interface Persona{
+    Nombre:string;
+    Apellido:string;
+    Acercade:string;
+    Titulos:string;
+
+    }
+
+let portafolio:Persona={
+    Nombre:"",
+    Apellido:"",
+    Acercade:"",
+    Titulos:["",""],
+}
+function GetNombre(nombre:string) {
+    
+}
+}
