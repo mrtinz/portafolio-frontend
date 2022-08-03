f49b9d32f9d6fc3e163b3693e9cd7ee830a0e580
mrtinz <melmrtinz@gmail.com>
2022-08-03 12:38:21 -0300

    okay va queriendo

--- proyecto/src/app/app.module.ts
+++ proyecto/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+import { InfoComponent } from './componentes/info/info.component';
+import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EncabezadoComponent
+    EncabezadoComponent,
+    InfoComponent,
+    AcercaDeComponent
   ],
   imports: [
     BrowserModule,
