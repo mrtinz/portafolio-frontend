5573d9a1f5093672f88198a8361bbba40c38fe33
mrtinz <melmrtinz@gmail.com>
2022-08-03 15:14:58 -0300

    masomenos

--- proyecto/src/app/app.module.ts
+++ proyecto/src/app/app.module.ts
@@ -3,16 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
-import { InfoComponent } from './componentes/info/info.component';
-import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    EncabezadoComponent,
-    InfoComponent,
-    AcercaDeComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
