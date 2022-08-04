d83684e4b3c9bae25ae3f8cd9365bd431414c883
mrtinz <melmrtinz@gmail.com>
2022-08-04 11:47:13 -0300

    componentes creados

--- proyecto/src/app/app.module.ts
+++ proyecto/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { InfoComponent } from './componentes/info/info.component';
+import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
+import { AptitudesComponent } from './componentes/aptitudes/aptitudes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    InfoComponent,
+    AcercaDeComponent,
+    AptitudesComponent
   ],
   imports: [
     BrowserModule,
