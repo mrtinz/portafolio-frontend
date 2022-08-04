a3c71e6ac1e08e85c93a458e6c46bdcf493aba0e
mrtinz <melmrtinz@gmail.com>
2022-08-04 14:47:57 -0300

    listo el modulo

--- proyecto/src/app/componentes/header/header.component.ts
+++ proyecto/src/app/componentes/header/header.component.ts
@@ -10,12 +10,13 @@ export class HeaderComponent implements OnInit {
   
   
   
-
+miPorfolio:any;
 
   constructor(private datosPorfolio:PorfolioService) { }
 
   ngOnInit(): void {
    this.datosPorfolio.obtenerDatos().subscribe(data => {console.log(data);
+    this.miPorfolio=data;
   });
   }
 
